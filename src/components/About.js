189a8ee837e3fa4cf13439209fd92f59fd667a4e
Ashraf <54310457+afaithraf@users.noreply.github.com>
2019-12-18 23:33:01 +0100

    beta

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,10 @@ export default function About() {
             <span>about</span>
             <p style={{ fontSize: "1.2em", margin: "20px auto", maxWidth: "90%", textAlign: "left", lineHeight: "1.7em" }}>
                 Welcome to Quotify, <br />
-                Quotify is a client side application to diplay random tech quotes supplied by this<a target="_blank" href="http://quotes.stormconsultancy.co.uk/random.json"> public api</a><br />
-                This was made by <a target="_blank" href="https://github.com/afaithraf">Ashraf</a><br />
+                Quotify is a client side application to diplay random tech quotes supplied by this<a target="_blank" rel="noopener noreferrer" href="http://quotes.stormconsultancy.co.uk/random.json"> public api</a><br />
+                This was made by <a target="_blank" rel="noopener noreferrer" href="https://github.com/afaithraf">Ashraf</a><br />
                 <br />
-                    Feel free to use <a target="_blank" href="https://github.com/afaithraf/quotify">Source code</a> 
+                    Feel free to use <a target="_blank" rel="noopener noreferrer" href="https://github.com/afaithraf/quotify">Source code</a> 
                 <br />
                 MIT License | Copyright (c) 2019 Ashraf<br />
             </p>
