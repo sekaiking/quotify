ee073694f92c44df39738ecf138a95a27d9252c2
Ashraf <54310457+afaithraf@users.noreply.github.com>
2019-12-18 13:30:29 +0100

    Updates

---
+++ precache-manifest.28a03c3443dc901fd40ea84dc457b640.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e31e5dbd60a615cee3f3e4bdbe9837c",
+    "url": "/quotify/index.html"
+  },
+  {
+    "revision": "aeb6083b8d6bdeba6355",
+    "url": "/quotify/static/css/main.585f20ff.chunk.css"
+  },
+  {
+    "revision": "b40287c724a21835c6d2",
+    "url": "/quotify/static/js/2.50248c9d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/quotify/static/js/2.50248c9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aeb6083b8d6bdeba6355",
+    "url": "/quotify/static/js/main.b531d750.chunk.js"
+  },
+  {
+    "revision": "6d53c75b107f0a27a94d",
+    "url": "/quotify/static/js/runtime-main.e3f3128a.js"
+  },
+  {
+    "revision": "27e2ef144c05cb180a1dc9c34fa9f3d6",
+    "url": "/quotify/static/media/loading.27e2ef14.svg"
+  },
+  {
+    "revision": "f3c971f088998dc904c3f69a7e64aab2",
+    "url": "/quotify/static/media/logo.f3c971f0.png"
+  }
+]);
